4074fd6ab1f88ace167225045d32d40027083ccb
dm-duys <duan.uys@data-methods.com>
2024-03-02 22:33:11 -0600

    renamed project to be more generic and bump version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="pygcu",
+    name="redutils",
     version="0.3.1",
-    python_requires=">3.10.0",
+    python_requires=">3.11.0",
     packages=find_packages(where=".", exclude=["./docs", "./.venv"]),
     description="A collection of utilities and apis related to gcu",
 )
