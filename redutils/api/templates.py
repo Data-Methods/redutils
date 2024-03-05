4d38ca992a45775388fa961af2a8cadaf6e0d476
dm-duys <duan.uys@data-methods.com>
2024-03-05 11:01:55 -0600

    added default run method for ingestion template

--- redutils/api/templates.py
+++ redutils/api/templates.py
@@ -56,32 +56,22 @@ class IngestionTemplate:
         """pre-hook logic before actual ingestion takes place, use this to set things up"""
         raise NotImplementedError()
 
-    def extract(
-        self, apply_func: Callable[[pd.DataFrame], pd.DataFrame] | None = None
-    ) -> pd.DataFrame:
+    def extract(self) -> pd.DataFrame:
         """overwrite method that handles actual pulling of data
 
-
-        :param apply_func: (Callable) - A passable function that accepts a DataFrame and return the
-        the same DataFrame. Use this function to do some post-processing of the data.
-
         :return: `DataFrame`_
 
         .. _DataFrame: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
         """
-
-    def post_process(self, df: pd.DataFrame) -> pd.DataFrame:
-        """apply custom post processing of retrieved dataframe
-
-        :param df: (pd.DataFrame) - The original dataframe retrieved from extraction
-
-        :return: `DataFrame`_
-        .. _DataFrame: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
-
-        """
-        return df
 
     def post_extract(self, df: pd.DataFrame) -> None:
         """post-hook logic after ingestion of data. Override this method
         for any updates to database or cleanup work.
         """
+
+    def run(self) -> None:
+        """Run the ingestion process"""
+        self.pre_extract()
+        df = self.extract()
+        self.post_extract(df)
+        return df
