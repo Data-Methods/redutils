729cc3960cb0c0e855167b06e649ce9512ba3514
dm-duys <duan.uys@data-methods.com>
2024-03-07 12:10:03 -0600

    ``` Add version file and update setup to use dynamic versioning
    Added:
    - VERSION.txt to track the project version.
    - Function get_version in setup.py to read the project version from VERSION.txt.
    
    Changed:
    - Updated setup.py to use the get_version function for dynamic versioning.
    - Added setuptools to requirements.txt for package management.
    
    Removed:
    - Static version string replaced with dynamic versioning in setup.py.
    ```

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from struct import pack
 from setuptools import setup, find_packages
 
 
+def get_version():
+    with open("VERSION.txt") as f:
+        return f.read().strip()
+
+
 setup(
     name="redutils",
-    version="0.4.0",
+    version=get_version(),
     python_requires=">3.11.0",
     packages=find_packages(where=".", exclude=["./docs", "./.venv"]),
     description="A collection of utilities and apis related to gcu",
